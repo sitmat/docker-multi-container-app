bef3a54d0188b654b4402e8250fcaf81bbf864b9
Mateusz Sito <sitomateusz@interia.pl>
2023-01-27 23:22:53 +0100

    Fetching Data in React App

---
+++ client/src/Fib.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import axios from 'axios';
+
+class Fib extends Component {
+    state = {
+        seenIndexes: [],
+        values: {},
+        indent: ''
+    };
+
+    componentDidMount() {
+        this.fetchValues();
+        this.fetchIndexes();
+    }
+
+    async fetchValues() {
+        const values = await axios.get('/api/values/current');
+        this.setState({values: values.data});
+    }
+
+    async fetchIndexes() {
+        const seenIndexes = await axios.get('/api/values/all');
+        this.setState({
+            seenIndexes: seenIndexes.data
+        })
+    }
+
+
+}
