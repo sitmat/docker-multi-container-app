8f30a7ccbdf60d8c26b172b10080ed77dab8dde4
Mateusz Sito <sitomateusz@interia.pl>
2023-01-30 22:43:40 +0100

    Dockerizing React app

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default () => {
+const OtherPage = () => {
     return (
         <div>
-            Im some oter page!
+            Im some other page!
             <Link to="/">Go back home</Link>
         </div>
     );
 };
+
+export default OtherPage;
